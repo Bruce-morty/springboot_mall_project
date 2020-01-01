dbf965c30a8fac71c73b6bbdfa8f3c58cf818948
clien9025 <302677599@qq.com>
2020-01-02 00:17:44 +0800

    tcs da han pi

--- src/main/java/top/philxin/controller/wx/WxSearchController.java
+++ src/main/java/top/philxin/controller/wx/WxSearchController.java
@@ -3,9 +3,11 @@ package top.philxin.controller.wx;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import top.philxin.exception.WxBusyException;
 import top.philxin.model.responseModel.CommonsModel.BaseRespVo;
 import top.philxin.service.wx.WxSearchService;
 
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,11 +24,40 @@ public class WxSearchController {
     @Autowired
     WxSearchService wxSearchService;
 
+    /**
+     * 搜取获取关键字
+     *
+     * @return
+     */
     @RequestMapping("search/index")
     public BaseRespVo search() {
-        BaseRespVo<Object> baseRespVo = new BaseRespVo<>();
         Map map = wxSearchService.queryHistory();
-//        baseRespVo.setData(map);
         return BaseRespVo.success(map);
     }
+
+    /**
+     * 清除搜索历史
+     *
+     * @return
+     */
+    @RequestMapping("search/clearhistory")
+    public BaseRespVo clear() {
+        int i = wxSearchService.cleanHistorySearch();
+        if (i == 0) {
+            throw new WxBusyException();
+        }
+        return BaseRespVo.success();
+    }
+
+    /**
+     * 联想查找
+     *
+     * @return
+     */
+    @RequestMapping("search/helper")
+    public BaseRespVo helper(String keyword) {
+        List<String> strings = wxSearchService.associationHelper(keyword);
+        return BaseRespVo.success(strings);
+    }
+
 }
